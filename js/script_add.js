6260c3dfde50ceaac1c7c25e1ebe3bc68a54edd5
Ismail-bfq <ismailbouifsquane@gmail.com>
2026-04-22 13:55:41 +0100

    add profile page and a page for sellers

---
+++ js/script_add.js
@@ -0,0 +1,28 @@
+function validationForm(){
+    //consts
+    const bikeName = document.getElementById("bikeName").value;
+    const price =document.getElementById(price).value;
+    const seller =document.getElementById("seller").value;
+    const phone =document.getElementById("phone").value;    
+    const image =document.getElementById("image").value;
+    // allerts of input not added
+    if (image === ""){
+        alert("pleas choose a bike image ");
+        return false
+    }
+    if (bikeName === ""){
+        alert("pleas choose a bike name");
+        return false
+    }
+    if (price === "" ||price<=0){
+        alert("price not given or invalid value ");
+        return false
+    }
+    iif (phone.length < 10){
+        alert("pleas choose a valid phone number!! ");
+        return false
+    }
+    // bike added
+    alert("Bike ADDed Successfully ✅")
+
+}
